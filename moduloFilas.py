64e18f5fc1f13692d394809531eade453b7a9793
VSSantana <sousavinicius@hotmail.com>
2019-11-16 18:14:31 -0300

    Atualização de versão.

--- moduloFilas.py
+++ moduloFilas.py
@@ -1,5 +1,5 @@
 """
-    Módulo responsável pelo gerenciamento da filas.
+    Módulo responsável pelo gerenciamento das filas.
     Mantém as interfaces e funções que operam sobre os processos.
 """
 
@@ -10,7 +10,6 @@ class Fila:
 
         self.lista_processo_usuario_pronto = None
         self.lista_entrada_saida = None
-        self.fila = None
         self.lista_processo_0 = []
         self.lista_processo_1 = []
         self.lista_processo_2 = []
@@ -28,10 +27,12 @@ class Fila:
         self.lista_processo_sistema_execucao = []
              
     def ordenar_filas_prioridade(self, tempo_execucao):
+
         """
-            Método responsável por ordenar os processos
+            Método responsável por ordenar os processos processos 
             na fila de prioridade, em ordem crescente de menor para maior.     
         """
+
         for processo in self.lista_processo_pronto:          
               
             if processo.tempo_inicializacao <= tempo_execucao:
@@ -46,7 +47,7 @@ class Fila:
                     self.lista_processo_2.append(processo)
                     
                 if processo.prioridade == 3:
-                    self.fila.lista_processo_3.append(processo)
+                    self.lista_processo_3.append(processo)
 
     def alterar_fila_prioridade_usuario(self, tempo_execucao):
         """
@@ -57,19 +58,19 @@ class Fila:
         """       
         for processo in self.lista_processo_2:
           
-            if(processo.execucao == 0) and (processo.tempo_inicializacao + 9 < tempo_execucao) and \
-                    not processo.lista_prioridade:
+            if((processo.execucao == 0) and (processo.tempo_inicializacao + 9 < tempo_execucao) and
+                    (not processo.lista_prioridade)):
                 processo.lista_prioridade = True
                 self.lista_processo_1.append(processo)
-            elif(processo.execucao == 0) and (processo.tempo_inicializacao + 19 < tempo_execucao) and \
-                    processo.lista_prioridade:
+            elif((processo.execucao == 0) and (processo.tempo_inicializacao + 19 < tempo_execucao) and
+                 processo.lista_prioridade):
                 self.lista_processo_1.append(processo)
        
         for processo in self.lista_processo_3:
-            if (processo.execucao == 0) and (processo.tempo_inicializacao + 9 < tempo_execucao):
-                if not processo.lista_prioridade:
-                    processo.lista_prioridade = True
-                    self.lista_processo_2.append(processo)
+            if((processo.execucao == 0) and (processo.tempo_inicializacao + 9 < tempo_execucao) and
+                    (not processo.lista_prioridade)):
+                processo.lista_prioridade = True 
+                self.lista_processo_2.append(processo)
 
     def adicionar_processo_fila_prioridade_usuario(self):
         """
